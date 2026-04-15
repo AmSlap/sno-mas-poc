ca9e27f7c3e08a695967ecdd9a0384e875ceca19
Sabouni Mohamed Ayoub <hindayoub.as@gmail.com>
2026-04-15 11:14:30 +0100

    deprecated ipv4 config fix

--- terraform/modules/ibm/compute/outputs.tf
+++ terraform/modules/ibm/compute/outputs.tf
@@ -5,7 +5,7 @@ output "id" {
 
 output "private_ip" {
   description = "Primary NIC private IPv4."
-  value       = ibm_is_instance.this.primary_network_interface[0].primary_ipv4_address
+  value       = ibm_is_instance.this.primary_network_interface[0].primary_ip[0].address
 }
 
 output "name" {
